5048540be808d7426a54cb1d8e876b948a890964
Hy <krishucn@gmail.com>
2023-05-26 14:08:58 +0800

    firstUpdate

---
+++ app/src/main/java/com/xylitol/luckydraw/LuckyDrawView.kt
@@ -0,0 +1,101 @@
+package com.xylitol.luckydraw
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @Author xylitol
+ * @Date 2020-11-26 15:47
+ * @Desc 继承RecyclerView完成一个3X3的抽奖转盘
+ */
+class LuckyDrawView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    private val animator = ValueAnimator()
+    private var mLuckyIndex = 4//最终抽中的位置
+    private var lotteryStatus = 0//当前可以抽奖状态
+    private var oldIndex = 0
+    private var mLotteryListener: LotteryListener? = null
+
+    private var mAdapter: LuckyDrawAdapter? = null
+
+    init {
+        animator.duration = 4000
+        animator.setIntValues(0, 4 * 8 + mLuckyIndex)
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.addUpdateListener {
+            val position = it.animatedValue as Int
+            setCurrentPosition(position % 8)
+        }
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                setCurrentPosition(mLuckyIndex)
+                lotteryStatus = 0
+                mLotteryListener?.onEnd()
+            }
+        })
+
+        layoutManager = object : GridLayoutManager(context, 3) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+    }
+
+    /**
+     * 传入后台数据
+     * @param data 后台数据、传入到adapter中去
+     */
+    fun setAdapterAndListener(data: Any,lotteryListener: LotteryListener) {
+        mLotteryListener = lotteryListener
+        mAdapter = LuckyDrawAdapter(mLotteryListener!!/*,data*/)
+        this.adapter = mAdapter
+    }
+
+    /**
+     * 开始抽奖
+     * @param luckyIndex 最后中奖位置
+     */
+    fun startLottery(luckyIndex: Int){
+        if (lotteryStatus == 0) {
+            if (luckyIndex in 0..7) {
+                mLuckyIndex = luckyIndex
+                animator.setIntValues(0, 4 * 8 + mLuckyIndex)
+            }
+            animator.start()
+            lotteryStatus = 1
+        }
+    }
+
+
+    /**
+     * 恢复到未抽奖状态
+     */
+    fun resetLottery() {
+        mAdapter?.resetAll()
+    }
+
+    private fun setCurrentPosition(position: Int) {
+        //刷新当前所在位置
+        mAdapter?.setSelectionPosition(position)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        animator.cancel()
+    }
+
+    interface LotteryListener {
+        fun onClickLottery()
+        fun onEnd()
+    }
+
+}
---
+++ app/src/main/java/com/xylitol/luckydraw/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.xylitol.luckydraw
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var iconArray = arrayOf(
+            R.mipmap.ac0,
+            R.mipmap.ac1,
+            R.mipmap.ac2,
+            R.mipmap.ac3,
+            R.mipmap.ac4,
+            R.mipmap.ac5,
+            R.mipmap.ac6,
+            R.mipmap.ac7
+        )
+    }
+
+    private var mLuckyDrawView: LuckyDrawView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mLuckyDrawView = findViewById(R.id.ldv_top)
+
+
+        //data 传入后台给的抽奖数据
+        mLuckyDrawView?.setAdapterAndListener("", object : LuckyDrawView.LotteryListener {
+            override fun onClickLottery() {
+                //点击开始抽奖调用抽奖接口后开始抽奖动画
+                val result = Random.nextInt(0, 7) //模拟抽奖接口
+                mLuckyDrawView?.startLottery(result)
+            }
+
+            override fun onEnd() {
+                //结束后恢复原样
+                Handler(Looper.getMainLooper()).postDelayed({
+                    mLuckyDrawView?.resetLottery()
+                }, 2000L)
+            }
+        })
+    }
+}
